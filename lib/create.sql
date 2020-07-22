6a6459123ab86991924356ebe0f21433fe1875c5
afteralec <github email address>
2020-07-22 14:01:06 -0700

    Commit with tables created.

---
+++ lib/create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE projects
+(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE user
+(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge
+(
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  project_id INTEGER
+);
